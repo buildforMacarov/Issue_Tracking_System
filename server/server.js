d390d339d8cd1e3c950e83392c24eae774fe3b18
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-19 23:11:24 +0530

    use bodyparser mdlware and create routes for testing

--- server/server.js
+++ server/server.js
@@ -23,20 +23,32 @@ const connection = mysql.createConnection({
 // const connection = mysql.createConnection(`mysql://${USER}:${PASSWORD}@${HOST}:${PORT}/${DB}`);
 
 // Middleware:
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
 connection.connect(error => {
 	if (error) throw error;
 	console.log('Connected!');
 });
-connection.query('DESCRIBE Animal', printResponse);
-connection.query('SELECT * FROM Animal', printResponse);
-connection.end();
 
-function printResponse(error, results, fields) {
-	if (error) throw error;
-	console.log('Results:\n' + JSON.stringify(results, null, 4));
-	console.log('Fields:\n' + JSON.stringify(fields, null, 4));
+app.get('/getTables', (req, res) => {
+	connection.query(`SHOW TABLES`, handleData(res));
+});
+
+app.get('/get/:table', (req, res) => {
+	connection.query(`SELECT * FROM ${req.params.table}`, handleData(res));
+});
+
+app.get('/describe/:table', (req, res) => {
+	connection.query(`DESCRIBE ${req.params.table}`, handleData(res));
+});
+
+function handleData(res) {
+	return (error, results, fields) => {
+		if (error) return res.redirect('https://http.cat/400');
+		res.json(results);
+	}
 }
 
 const server = app.listen(3000, () => {
