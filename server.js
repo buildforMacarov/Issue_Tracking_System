0fb5926eba2f4b382c384943c933d999d39c8c82
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-19 16:47:43 +0530

    install mysql, dotenv; test mysql connection to zoo database

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const USER = 'root',
+	PASSWORD = process.env.PASSWORD;
+	HOST = 'localhost',
+	PORT = '3306',
+	DB = 'zoo';
+
+const connection = mysql.createConnection({
+	user: USER,
+	password: PASSWORD,
+	host: HOST,
+	database: DB
+});
+
+// Alternative:
+// const connection = mysql.createConnection(`mysql://${USER}:${PASSWORD}@${HOST}:${PORT}/${DB}`);
+
+connection.connect(error => {
+	if (error) throw error;
+	console.log('Connected!');
+});
+connection.query('DESCRIBE Animal', printResponse);
+connection.query('SELECT * FROM Animal', printResponse);
+connection.end();
+
+function printResponse(error, response) {
+	if (error) throw error;
+	console.log('Response:\n' + JSON.stringify(response, null, 4));
+}
