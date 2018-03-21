9438b5cf2ee8e5d3569ad0a33a8c62dede77476e
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-22 00:21:32 +0530

    fix - removed components from app.js

--- client/app.js
+++ client/app.js
@@ -1,63 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class IssueCard extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { open: true }
-
-		this.toggleOpen = this.toggleOpen.bind(this);
-	}
-
-	toggleOpen() {
-		this.setState(prevState => ({
-			open: !prevState.open
-		}));
-	}
-
-	render() {
-		return (
-			<div className="card">
-				<div className="card-body">
-					<h5 className="card-title">{this.props.heading}</h5>
-					<h6 className="card-subtitle">Opened by {this.props.opener}</h6>
-					<p className="card-text">{this.props.body}</p>
-					<button
-						type="button"
-						className={this.state.open ? 'btn btn-success' : 'btn btn-danger'}
-						onClick={this.toggleOpen}>
-						{this.state.open ? 'Open' : 'Closed'}
-					</button>
-				</div>
-			</div>
-		);
-	}
-}
-
-class IssueGrid extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					{
-						this.props.cardTexts.map(text => (
-							<div className="col">
-								<IssueCard
-									heading={text.heading}
-									body={text.body}
-									opener={text.opener}
-								/>
-							</div>
-						))
-					}
-				</div>
-			</div>
-		);
-	}
-}
+import { IssueGrid } from './components/issuegrid';
 
 const cardTexts = [
 	{
