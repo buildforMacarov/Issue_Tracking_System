9744742e43425e0bfc0d9de461b569d948f44464
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-26 17:47:37 +0530

    test navbar and userpage components

--- client/app.js
+++ client/app.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { IssueGrid } from './components/issuegrid';
+import { UserPage } from './components/userpage';
 
 ReactDOM.render(
-	<IssueGrid />,
+	<UserPage />,
 	document.getElementById('root')
 );
