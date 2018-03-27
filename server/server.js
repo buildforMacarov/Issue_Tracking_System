fd6336884286cf50c8b8bfc7af72022dcf7b4a84
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-27 16:39:49 +0530

    Create GET routes: - GET /issues - GET /users - GET /users/:userId/issues
    Install lodash for handling querystring
    And create test-db.sql file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const _ = require('lodash');
 
 const app = express();
 
@@ -32,24 +33,47 @@ connection.connect(error => {
 	console.log('Connected!');
 });
 
-app.get('/getTables', (req, res) => {
-	connection.query(`SHOW TABLES`, handleData(res));
+app.get('/issues', sqlWhere, (req, res) => {
+	const query = `
+		SELECT * FROM issues ${req.sqlWhere}
+	`;
+	connection.query(query, sendData(res));
 });
 
-app.get('/get/:table', (req, res) => {
-	connection.query(`SELECT * FROM ${req.params.table}`, handleData(res));
+app.get('/users', sqlWhere, (req, res) => {
+	const query = `
+		SELECT * FROM users ${req.sqlWhere}
+	`;
+	connection.query(query, sendData(res));
 });
 
-app.get('/describe/:table', (req, res) => {
-	connection.query(`DESCRIBE ${req.params.table}`, handleData(res));
+app.get('/users/:userId/issues', (req, res) => {
+	const userId = req.params.userId;
+	const query = `
+		SELECT issues.*
+		FROM users INNER JOIN user_issues ON users.id = user_issues.user_id
+		INNER JOIN issues ON user_issues.issue_id = issues.id
+		WHERE users.id = ${connection.escape(userId)}
+	`;
+	connection.query(query, sendData(res));
 });
 
-function handleData(res) {
+function sendData(res) {
 	return (error, results, fields) => {
 		if (error) return res.redirect('https://http.cat/400');
 		res.json(results);
 	}
 }
+
+function sqlWhere(req, res, next) {
+	req.sqlWhere = '';
+	if (!_.isEmpty(req.query)) {
+		const key = _.keys(req.query)[0];
+		const value = req.query[key];
+		req.sqlWhere += `WHERE ${key} = ${connection.escape(value)}`;
+	}
+	next();
+};
 
 const server = app.listen(3000, () => {
 	console.log('Listening at port 3000...');
