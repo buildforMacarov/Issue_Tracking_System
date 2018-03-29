a35ff6f9c34bbc6fc3c1ee14cdec9d8ff8956475
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-29 16:19:37 +0530

    create tests for GET /users/:userId/issues

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -16,11 +16,10 @@ describe('GET /issues', () => {
 	});
 });
 
-describe('GET /issues/id', () => {
+describe('GET /issues/:id', () => {
 	it('should return an issue', (done) => {
-		const id = 3;
 		request(app)
-			.get(`/issues/${id}`)
+			.get('/issues/3')
 			.expect(200)
 			.expect(res => {
 				const issue = res.body.rows[0];
@@ -58,7 +57,7 @@ describe('GET /users', () => {
 	});
 });
 
-describe('GET /users/id', () => {
+describe('GET /users/:id', () => {
 	it('should return a user', (done) => {
 		request(app)
 			.get('/users/5')
@@ -81,6 +80,34 @@ describe('GET /users/id', () => {
 	it('should return 404 if id not found', (done) => {
 		request(app)
 			.get('/users/9999')
+			.expect(404)
+			.end(done);
+	});
+});
+
+describe('GET /users/:userId/issues', () => {
+	it('should return all of a user\'s issues', (done) => {
+		request(app)
+			.get('/users/3/issues')
+			.expect(200)
+			.expect(res => {
+				const issueIds = res.body.rows.map(issue => issue.id);
+				expect(res.body.rows.length).toBe(3);
+				expect(issueIds).toEqual([2, 3, 7]);
+			})
+			.end(done);
+	});
+
+	it('should return 404 if userId invalid', (done) => {
+		request(app)
+			.get('/users/abc/issues')
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if userId not found', (done) => {
+		request(app)
+			.get('/users/9999/issues')
 			.expect(404)
 			.end(done);
 	});
