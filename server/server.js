5640eb6901e7fd058384824b42b0bbdba00c49eb
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-04-08 21:43:54 +0530

    use developer router

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const Issue = require('./models/issue');
 
 const userRouter = require('./routes/user');
 const issueRouter = require('./routes/issue');
+const developerRouter = require('./routes/developer');
 
 const logger = require('./middleware/logger');
 const { authenticateUser } = require('./middleware/authenticate');
@@ -30,47 +31,7 @@ app.use(express.static(__dirname + '/../public'));
 
 app.use('/issues', issueRouter);
 app.use('/users', userRouter);
-
-app.get('/developers', (req, res) => {
-	Developer.findAll()
-		.then(devs => {
-			if (devs.length === 0) {
-				return res.status(404).send();
-			}
-			devs = devs.map(dev => dev.toPublic());
-			res.json({ developers: devs });
-		})
-		.catch(error => res.status(400).send());
-});
-
-app.get('/developers/:id', (req, res) => {
-	Developer.findById(req.params.id)
-		.then(dev => {
-			if (!dev) {
-				return res.status(404).send();
-			}
-			dev = dev.toPublic();
-			res.json({ developer: dev });
-		})
-		.catch(error => res.status(400).send());
-});
-
-app.get('/developers/:developerId/issues', (req, res) => {
-	Developer.findById(req.params.developerId)
-		.then(dev => {
-			if (!dev) {
-				return res.status(404).send();
-			}
-			return dev.findAllIssues();
-		})
-		.then(issues => {
-			if (issues.length === 0) {
-				return res.status(404).send();
-			}
-			res.json({ issues });
-		})
-		.catch(error => res.status(400).send());
-});
+app.use('/developers', developerRouter);
 
 app.get('/admins', (req, res) => {
 	Admin.findAll()
