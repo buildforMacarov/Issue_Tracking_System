e7ae63eadfeb87b21286a27eab2727eee3a02e18
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-04-18 23:02:20 +0530

    create GET /admins/issues with tests

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -23,6 +23,29 @@ router.get('/me', authenticateAdmin, (req, res) => {
 	res.json({ admin });
 });
 
+router.get('/issues', authenticateAdmin, async (req, res) => {
+	try {
+		const issues = await req.admin.findAllIssues();
+		if (issues.length === 0) return res.status(404).send();
+
+		const raiserGroupPromises = issues.map(issue => issue.getRaisers());  // Array<Promise<Array<JSON>>>
+		const assigneeGroupPromises = issues.map(issue => issue.getAssignees());  // Array<Promise<Array<JSON>>>
+
+		const raiserGroups = await Promise.all(raiserGroupPromises);
+		issues.forEach((issue, i) => {
+			issue.raisers = raiserGroups[i];  // Array<JSON>
+		});
+		const assigneeGroups= await Promise.all(assigneeGroupPromises);
+		issues.forEach((issue, i) => {
+			issue.assignees = assigneeGroups[i];  // Array<JSON>
+		});
+
+		res.json({ issues });
+	} catch (error) {
+		res.status(400).send();
+	}
+});
+
 router.get('/:id', (req, res) => {
 	Admin.findById(req.params.id)
 		.then(admin => {
