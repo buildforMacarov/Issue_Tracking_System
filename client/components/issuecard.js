71bf58f0307ca46be8f80e05477697fc01eaf1e0
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-04-19 20:43:32 +0530

    issuecard time and status format

--- client/components/issuecard.js
+++ client/components/issuecard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import moment from 'moment';
 
 export class IssueCard extends React.Component {
 	render() {
@@ -12,9 +12,27 @@ export class IssueCard extends React.Component {
 					<span>#{id}</span>
 				</div>
 				<div className="Issue-card__body">
-					<p>{description}</p>
+					<p className="Issue-card__desc">{description}</p>
+				</div>
+				<div className="Issue-card__footer d-flex w-100 justify-content-between">
+					<p className="Issue-card__time text-muted">{formattedDateTime(time)}</p>
+					<p className={`Issue-card__status Issue-card__status--${status}`}>{status}</p>
 				</div>
 			</div>
 		);
 	}
+}
+
+function formattedDateTime(time) {
+	const input = moment(time);
+	const now = moment();
+	const week = 604800000;
+	const cap = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+	const fDate = now.diff(input) <= week ?
+		cap(input.from(now)) :
+		input.format('DD MMM, YYYY');
+	const fTime = input.format('h:mm a');
+
+	return `${fDate} at ${fTime}`;
 }
